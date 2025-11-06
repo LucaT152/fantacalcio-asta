b6f5a30e873d2f82d7fea52bef05e0c3841b22c2
Luca <latorre.luca05@gmail.com>
2025-11-06 22:20:21 +0100

    messe dimesioni input nel login a 80% aggiustato la cambio di pagina

--- forntend/js/login.js
+++ forntend/js/login.js
@@ -1,19 +1,21 @@
 function mostraLogin() {
-  document.getElementById("form-login").classList.add("form-visibile");
-  document.getElementById("form-login").classList.remove("form-nascosto");
+  const formLogin = document.getElementById("form-login");
+  const formRegistrazione = document.getElementById("form-registrazione");
 
-  document.getElementById("form-registrazione").classList.add("form-nascosto");
-  document
-    .getElementById("form-registrazione")
-    .classList.remove("form-visibile");
+  formLogin.classList.add("form-visibile");
+  formLogin.classList.remove("form-nascosto");
+
+  formRegistrazione.classList.add("form-nascosto");
+  formRegistrazione.classList.remove("form-visibile");
 }
 
 function mostraRegistrazione() {
-  document.getElementById("form-registrazione").classList.add("form-visibile");
-  document
-    .getElementById("form-registrazione")
-    .classList.remove("form-nascosto");
+  const formLogin = document.getElementById("form-login");
+  const formRegistrazione = document.getElementById("form-registrazione");
 
-  document.getElementById("form-login").classList.add("form-nascosto");
-  document.getElementById("form-login").classList.remove("form-visibile");
+  formRegistrazione.classList.add("form-visibile");
+  formRegistrazione.classList.remove("form-nascosto");
+
+  formLogin.classList.add("form-nascosto");
+  formLogin.classList.remove("form-visibile");
 }
